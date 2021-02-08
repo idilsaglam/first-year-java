281e5aadddb671c5855cd60251df3fde8d0ba644
idilsaglam <idilsaglam1@hotmail.com>
2021-02-08 23:05:32 +0100

    Temperature updated

--- src/main/java/l1/s2/ip2/td/td2/Temperature.java
+++ src/main/java/l1/s2/ip2/td/td2/Temperature.java
@@ -30,4 +30,59 @@ public class Temperature {
         this.unite = t.unite;
     }
 
+    public void afficher(Temperature temperature){
+        System.out.println(temperature.temp + temperature.unite);
+    }
+
+    private void conversionKC(Temperature temperature){
+        if(!temperature.unite.equals("Kelvin")){
+            return;
+        }
+        temperature.temp -= 273.15;
+    }
+
+    private void conversionFC(Temperature temperature){
+        if(!temperature.unite.equals("Fahrenheit")){
+            return;
+        }
+        temperature.temp = 9./5 * temperature.temp + 32;
+    }
+
+    public double read(String unite){
+        if(this.unite.equals(unite)){
+            return this.temp;
+        }
+        return Double.NEGATIVE_INFINITY;
+    }
+
+    public boolean isEqual(Temperature t, Temperature t2){
+        if(t.temp == t2.temp && t.unite.equals(t2.unite)){
+            return true;
+        }
+        return false;
+    }
+
+    public Temperature plusBasseQue(Temperature t, Temperature t2){
+        if(isEqual(t,t2)){
+            return t;
+        };
+        if (t.unite.equals("Kelvin")) {
+            conversionKC(t);
+        }
+        if(t2.unite.equals("Kelvin")){
+            conversionKC(t2);
+        }
+        if(t.unite.equals("Fahrenheit")){
+            conversionFC(t);
+        }
+        if(t2.unite.equals("Fahrenheit")){
+            conversionFC(t2);
+        }
+        if(t.temp > t2.temp){
+            return t;
+        }
+        return t2;
+    }
+
+
 }
