dfa11225769addf1de297b01bd3952e3dfc062f7
idilsaglam <idilsaglam1@hotmail.com>
2021-02-09 16:28:21 +0100

    Main class updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
-import l1.s2.ip2.td.td2.Test;
+import l1.s2.ip2.tp.tp1.Test;
+
 
 public class Main {
     public static void main(String[] args){
         System.out.println("IP2-TD2");
-        Test.main(args);
+        l1.s2.ip2.td.td2.Test.main(args);
+        System.out.println("IP2-TP2");
+        l1.s2.ip2.tp.tp2.Test.main(args);
+
     }
 }
