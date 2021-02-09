d7673c01a99c4f7a06c91c13b3dacf5312a07471
idilsaglam <idilsaglam1@hotmail.com>
2021-02-09 15:46:23 +0100

    Confiture class updated

--- src/main/java/l1/s2/ip2/td/td2/Confiture.java
+++ src/main/java/l1/s2/ip2/td/td2/Confiture.java
@@ -2,34 +2,33 @@ package l1.s2.ip2.td.td2;
 
 public class Confiture {
     private final String fruit;
-    private final int proportion;
-    private final int cal;
-    private final int sucre;
+    private int proportion;
+    private int cal;
+    private int sucre;
 
-    Confiture(String fruit, int proportion, int cal){
+    Confiture(String fruit, int proportion, int cal) {
         this.fruit = fruit;
         this.proportion = proportion;
         this.cal = cal;
-        this.sucre = 387 / 100 * this.proportion;
+        this.sucre = 387 * this.proportion / 100;
     }
 
-    Confiture(String nature,int cal){
-        this.fruit = nature;
-        this.cal = cal;
-        proportion = 50;
-        this.sucre = 387 / 100 * this.proportion;
+    Confiture(String nature, int cal) {
+        this(nature, 50, cal);
     }
-    public void description(){
-        System.out.printf(
-                "Confiture de %s, %d%% de fruit, %d calories aux 100 grammes.\n",
+
+    public String description(){
+        return String.format(
+                "Confiture de %s, %d%% de fruit, %d calories aux 100 grammes.",
                 this.fruit,
                 this.proportion,
                 this.cal
         );
     }
     public int calculerCal(int gramme){
-        return (gramme*100)/50;
+        return this.cal / 100 * gramme;
     }
+
     public boolean egal(Confiture c){
         return (
             c.fruit.equals(this.fruit) &&
@@ -41,9 +40,19 @@ public class Confiture {
     public String getFruit(){
         return this.fruit;
     }
+    public void setCal(int cal){
+        this.cal = cal;
+    }
 
-    public int setProportion(){
-        return this.proportion;
+    public void setProportion(int proportion){
+        this.proportion = proportion;
+
+    }
+
+    public int getSucre() {
+        return this.sucre;
     }
 
 }
+
+
