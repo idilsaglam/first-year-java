fa9651b1c53cf1db54af97286b92e0fedfe4c8bf
idilsaglam <idilsaglam1@hotmail.com>
2021-02-10 14:55:19 +0100

    Test class modified

--- src/main/java/l1/s2/ip2/tp/tp2/Test.java
+++ src/main/java/l1/s2/ip2/tp/tp2/Test.java
@@ -8,5 +8,24 @@ public class Test {
     System.out.println("nb d'etudiants: " + Etudiant.nombreDEtudiants);
     System.out.println("somme des notes: " + Etudiant.sommeDesNotes);
 
+    Etudiant idil = new Etudiant("Idil","Saglam",20.00);
+    System.out.printf("nb d'étudiants : %d somme des notes : %f\n",Etudiant.nombreDEtudiants,Etudiant.sommeDesNotes);
+
+    e1.afficher();
+
+    e2.afficher();
+    System.out.println("Note is modified to 19.5");
+    e2.modifierNote(19.5);
+    System.out.println("moyene : "+Etudiant.moyenne());
+
+    System.out.println(e1.estAdmis());
+    System.out.println(e2.estAdmis());
+    System.out.println(Etudiant.moyenne());
+    System.out.println(idil.meilleurQueLaMoyenne());
+    System.out.println(e1.meilleurQueLaMoyenne());
+
+    Trio trio = new Trio(e1,e2,idil);
+    System.out.println(trio.premier());
+
     }
 }
