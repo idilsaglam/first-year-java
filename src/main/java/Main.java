ae7150a393456902b7008179d693ba745d62e895
idilsaglam <idilsaglam1@hotmail.com>
2021-02-11 15:40:01 +0100

    Main class updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
         l1.s2.ip2.td.td2.Test.main(args);
         System.out.println("IP2-TP2");
         l1.s2.ip2.tp.tp2.Test.main(args);
+        System.out.println("IP2-TP3");
+        l1.s2.ip2.tp.tp3.Test.main(args);
 
     }
 }
