30518ad38a5d827a6ca518f3ec1114971c240d26
idilsaglam <idilsaglam1@hotmail.com>
2021-03-05 15:34:22 +0100

    TD1 updated

--- src/main/java/l1/s2/ci2/TD1.java
+++ src/main/java/l1/s2/ci2/TD1.java
@@ -2,59 +2,69 @@ package l1.s2.ci2;
 import java.util.Stack;
 
 public class TD1 {
-  public void afficher(Stack<Integer> s1){
-    return;
-  }
-  public void transvaser(Stack<Integer> s1, Stack<Integer> s2){
-    if(s1 == null || s2 == null){
+    public void afficher(Stack<Integer> s1) {
         return;
     }
-    while(!s1.empty()){
-        s2.push(s1.pop());
+
+    public void transvaser(Stack<Integer> s1, Stack<Integer> s2) {
+        if (s1 == null || s2 == null) {
+            return;
+        }
+        while (!s1.empty()) {
+            s2.push(s1.pop());
+        }
     }
-  }
-  public void deplacer(Stack<Integer> s1, Stack<Integer> s2){
-    if(s1 == null || s2 == null){
-        return;
+
+    public void deplacer(Stack<Integer> s1, Stack<Integer> s2) {
+        if (s1 == null || s2 == null) {
+            return;
+        }
+        transvaser(s1, s2);
+        Stack<Integer> s3 = new Stack<Integer>();
+        transvaser(s2, s3);
     }
-    transvaser(s1,s2);
-    Stack<Integer> s3 = new Stack<Integer>();
-    transvaser(s2,s3);
-  }
-  public void deplacerPairImpair(Stack<Integer> s1, Stack<Integer> pair){
 
-      if(s1 == null || pair == null){
-          return;
-      }
-      Stack<Integer> impair = new Stack<Integer>();
+    public void deplacerPairImpair(Stack<Integer> s1, Stack<Integer> pair) {
+        //TODO:
+        if (s1 == null || pair == null) {
+            return;
+        }
+        Stack<Integer> impair = new Stack<Integer>();
 
-      while(!s1.empty()){
-          int c = s1.pop();
-          if(c % 2 == 0){
-            pair.push(c);
-          }else{
-              impair.push(c);
-          }
-      }
+        while (!s1.empty()) {
+            int c = s1.pop();
+            if (c % 2 == 0) {
+                pair.push(c);
+            } else {
+                impair.push(c);
+            }
+        }
 
-      while (!pair.empty()){
-          impair.push(pair.pop());
-      }
-  }
+        while (!pair.empty()) {
+            impair.push(pair.pop());
+        }
+    }
 
-  public void copierPairs(Stack<Integer> s1, Stack<Integer> s2){
-      if(s1 == null || s2 == null){
-          return;
-      }
-      int c = s1.pop();
-      Stack<Integer> s3 = new Stack<Integer>();
-      while (!s1.empty()){
-          if(c % 2 == 0){
-              s2.push(c);
-              s3.push(c);
-          }
-      }
-  }
+    public static void copierPairs(Stack<Integer> s1, Stack<Integer> s2) {
+        if (s1 == null || s2 == null) {
+            return;
+        }
+
+        Stack<Integer> s3 = new Stack<Integer>();
+
+        while (!s1.empty()) {
+            int c = s1.pop();
+            if (c % 2 == 0) {
+                s2.push(c);
+            }
+            s3.push(c);
+        }
+
+        while (!s3.empty()) {
+            s1.push(s3.pop());
+        }
+    }
+
 
 
 }
