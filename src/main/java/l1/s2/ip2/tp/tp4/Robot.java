a263eee77eedf720d8918ee00d779dd19c1be9ea
idilsaglam <idilsaglam1@hotmail.com>
2021-03-10 00:29:35 +0100

    Robot class updated

--- src/main/java/l1/s2/ip2/tp/tp4/Robot.java
+++ src/main/java/l1/s2/ip2/tp/tp4/Robot.java
@@ -51,12 +51,22 @@ public class Robot {
         return this.nom;
     }
 
+    /**
+     * Méthode retourne l'énergie du robot actuel
+     * @return energie du robot
+     */
     public int getEnergie(){
         return this.energie;
     }
+
+    /**
+     * Méthode retourne le texte associé au robot
+     * @return texte associe au robot
+     */
     public String getTexte(){
         return this.texte;
     }
+
     /**
      * Comme l'attribut 'énergie' est privé, on ne peut pas y modifier dans les autres classes.
      * Pour pouvoir le modifier et garder son visibilité "private" en même temps, il faut que nous écrivons
@@ -69,11 +79,24 @@ public class Robot {
     /**
      * Question 3.5 : Après avoir chanté il perdra 10 points d’énergie (sans pouvoir passer en dessous de zéro).
      */
-    public void chante(){
+    public String chante(){
+        String res = "";
+        for(int i=0; i<this.getNum(); i++){
+            res+= this.getTexte();
+        }
         if(this.energie - 10 >= 0){
             this.energie -= 10;
-        }else{
+        } else {
             this.energie = 0;
         }
+        return res;
+    }
+
+    /**
+     * Méthode retourne la valeur entière pour le nom du robot
+     * @returns l'entier correspondant au numéro du robot
+     */
+    public int getNum() {
+        return this.nom - 'a' + 1;
     }
 }
