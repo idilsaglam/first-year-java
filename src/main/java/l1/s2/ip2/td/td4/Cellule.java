2f2a598cc2ab9bdd7f582ccd332c1550b3a366ae
idilsaglam <idilsaglam1@hotmail.com>
2021-03-10 14:26:02 +0100

    Animal class updated

--- src/main/java/l1/s2/ip2/td/td4/Cellule.java
+++ src/main/java/l1/s2/ip2/td/td4/Cellule.java
@@ -1,68 +1,91 @@
 package l1.s2.ip2.td.td4;
 
 public class Cellule {
-    private Animal content;
+    private Animal animal;
     private Cellule suivant;
 
-    public Cellule(String regime) {
-        // Intégrer un nouvel animal ayant un regime qui est passé en paramètre.
-        this.content = new Animal(regime);
-
-        this.suivant = null;
+    /**
+     * Question 1.1
+     * un constructeur recevant un argument de type String, et permet-
+     * tant d’intégrer un nouvel animal ayant ce régime.
+     * @param regime une chaîne de caractère qui signifie régime d'animal.
+     */
+    public Cellule(String regime){
+        this.animal = new Animal(regime);
     }
 
+    public Cellule getSuivant() {
+        return suivant;
+    }
 
-    public void ajouter2(String regime) {
-        // Ce n'est pas la dernière cellule de l'enclos
-        Cellule c = this;
-        while (c.suivant != null) {
-            c = c.suivant;
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public void setSuivant(Cellule suivant) {
+        this.suivant = suivant;
+    }
+
+    /**
+     * Question 1.4
+     * Une méthode pour compter le nombre d'animaux.
+     * @return res qui est le nombre d'animaux.
+     */
+    public int compteAnimal(){
+        Cellule actuel = this;
+        int res = 0;
+        while(actuel.suivant != null){
+            res++;
+            actuel = actuel.suivant;
         }
-        c.suivant = new Cellule(regime);
+        return res;
     }
 
-    public void ajouter(String regime) {
-        if (this.suivant == null) {
-            this.suivant = new Cellule(regime);
-            return;
-        }
-        this.suivant.ajouter(regime);
-    }
-
-    public int compteNombre(){
-        if(this.suivant == null){
-            return 1;
-        }
-        return this.suivant.compteNombre() + 1;
-    }
-
+    /**
+     * Question 1.5
+     *  une méthode transformant tous les animaux carnivores en herbivore et réciproquement.
+     */
     public void transformer(){
-        this.content.transformer();
-        if(this.suivant == null){
-            return;
+        Cellule actuel = this;
+        while(actuel.animal != null) {
+            actuel.animal.transformer();
+            actuel = actuel.suivant;
         }
-        this.suivant.transformer();
     }
 
+    /**
+     * Question 1.6
+     * Écrivez une méthode affichant le régime de tous les animaux de la liste (dans l’ordre qui vous convient).
+     */
     public void afficher(){
-        this.content.afficher();
-        if(this.suivant == null){
-            return;
+        Cellule actuel = this;
+        while(actuel.animal != null){
+            actuel.animal.afficher();
+            actuel = actuel.suivant;
         }
-        this.suivant.afficher();
     }
 
+    /*
+     * Question 2.1
+     * Donnez l’évolution de l’enclos lors des trois premiers tours de jeu sur les deux exemples:
+     * 1. tout herbivore ayant commencé le tour à côté d’un carnivore est mangé (et retiré de la liste)
+     * 2. une fois la règle précédente appliquée, si deux herbivores sont côte à côte, un nouvel herbivore
+     * est placé entre les deux, disponible pour le tour suivant.
+     */
     //TODO:
-    public void afficherDetail(){
-
-        this.content.afficher();
-        if(this.suivant == null){
-            return;
+    public void evolution(){
+        Cellule actuel = this;
+        while (actuel.suivant != null){
+            if(actuel.animal.getRegime().equals("carnivore") && actuel.suivant.animal.getRegime().equals("herbivore")){
+                // actuel.suivant.anmal i cikar
+            }
+            actuel = actuel.suivant;
         }
-        this.suivant.afficher();
     }
-
-
 
 
 }
--- src/main/java/l1/s2/ip2/td/td4/Enclos.java
+++ src/main/java/l1/s2/ip2/td/td4/Enclos.java
@@ -1,28 +1,46 @@
 package l1.s2.ip2.td.td4;
 
-public class Enclos{
-    Cellule debut;
-    Enclos() {
+public class Enclos {
+    private Cellule debut;
+
+    /**
+     * Question 1.1 :
+     * Un constructeur donnant un enclos vide.
+     */
+    public Enclos(){
         this.debut = null;
     }
 
-    public void ajouter(String regime) {
-        if (this.debut == null) {
-            // L'enclos est vide
-            this.debut = new Cellule(regime);
-            return;
-        }
-        // Si l'enclos n'est pas vide
-        this.debut.ajouter(regime);
+    /**
+     * Question 1.2
+     * une méthode ajouter qui prend en argument un régime alimentaire et qui ajoute un
+     * Animal au début d’un enclos.
+     * @param regime une chaîne de caractère qui signifie une regime d"un animal.
+     */
+    public void ajouter(String regime){
+        //Pour ne pas perdre le cellule actuel, on va le garder dans un attribut.
+        Cellule c = this.debut;
+        this.debut = new Cellule(regime);
+        this.debut.setSuivant(c);
     }
 
-    public void CompteAnimaux(){
+    /**
+     * Question 1.4
+     * une méthode ajouter qui prend en argument un régime alimentaire et qui ajoute un
+     * Animal au début d’un enclos.
+     * @return le nombre d'animal.
+     */
+    public int compteAnimal(){
         if(this.debut == null){
-            return;
+            return -1;
         }
-        this.debut.compteNombre();
+        return this.debut.compteAnimal();
     }
 
+    /**
+     * Question 1.5
+     * une méthode transformant tous les animaux carnivores en herbivore et réciproque- ment.
+     */
     public void transformer(){
         if(this.debut == null){
             return;
@@ -35,12 +53,6 @@ public class Enclos{
             return;
         }
         this.debut.afficher();
-    }
-
-    // TODO: ex 1.7
-
-    public void retire(Cellule c){
-
     }
 
 }
