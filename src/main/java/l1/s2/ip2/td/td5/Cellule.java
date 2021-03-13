4654caf940ffa06423b2cb88fe52e8bc98e54959
idilsaglam <idilsaglam1@hotmail.com>
2021-03-13 22:31:04 +0100

    Cellule class updated

--- src/main/java/l1/s2/ip2/td/td5/Cellule.java
+++ src/main/java/l1/s2/ip2/td/td5/Cellule.java
@@ -1,5 +1,7 @@
 package l1.s2.ip2.td.td5;
 
+import javax.swing.*;
+
 public class Cellule {
     private int valeur;
     private Cellule suivante;
@@ -46,13 +48,21 @@ public class Cellule {
      * une méthode description qui renvoie une chaîne de caractères décrivant la liste d’entiers séparés par des
      * espaces, ou "la liste est vide" si c’est le cas.
      */
-    public void description(){
-        System.out.println(this.valeur + "\s");
-        if(this.suivante != null){
-            this.suivante.description();
+    public String description(){
+        String res = this.valeur + "\s";
+        if(this.suivante == null){
+            return res;
         }
+        return this.suivante.description();
     }
 
+    public String descriptionRec(){
+        //cas d'arrêt
+        if(this.getSuivante() == null){
+            return this.getValeur()+")";
+        }
+        return this.getValeur() + ";" + this.getSuivante().descriptionRec();
+    }
     /**
      * Question 2.3 :
      * Une méthode taille (), sans argument, qui renvoie le nombre d’éléments de la liste.
@@ -101,21 +111,33 @@ public class Cellule {
 
     /**
      * Question 2.4
+     * Une méthode qui ajoute l’entier v en position i si elle existe, sinon elle l’ajoute en interprétant
+     * cet indice "au plus près" de ce qu’il peut signifier.
+     * @param i
+     * @param v
+     */
+    public void ajouterEni(int i, int v){
+
+    }
+
+    /**
+     * Question 2.5
      * Une méthode qui supprime le i-ème élément de la liste.
      * @param i un entier signifiant la position d'un élément que nous voulons supprimer de la liste.
      * @return true s'il est supprimé, sinon false.
      */
     public boolean supprimer_en_i(int i){
-        Cellule actuel = this;
-        for(int j= 0; j<i-1; j++){
-            if(actuel.suivante != null){
-            actuel = actuel.suivante;
-            }else{
-                return false;
-            }
-        }
-        actuel.suivante = actuel.suivante.suivante;
-        return true;
+       Cellule actuel = this;
+       while(actuel.suivante!= null){
+           if(actuel.suivante.getValeur() == i && actuel.suivante.suivante != null){
+               actuel.suivante= actuel.suivante.suivante;
+               return true;
+           }
+           if(actuel.suivante.getValeur() == i && actuel.suivante.suivante == null){
+               //suprimer
+           }
+       }
+       return  false;
     }
 
     public boolean supprimerEnIRecursive(int i){
@@ -123,17 +145,83 @@ public class Cellule {
             this.suivante = this.suivante.suivante;
             return true;
         }
-        if(this.suivante != null) {
-            this.suivante.supprimerEnIRecursive(i);
+        if(this.suivante == null){
+            return false;
+        }
+        return this.suivante.supprimerEnIRecursive(i);
+
+    }
+
+    /**
+     * Question 2.7 :
+     * Une méthode qui supprimera les k premières occurrences d’un entier v.
+     * Elle retournera le nombre de valeurs effectivement sup- primées.
+     * @param k
+     * @param v
+     * @return
+     */
+    public int supprimer_k_premieres_occ(int k, int v){
+        Cellule actuel = this;
+        int counter = 0;
+        while (actuel != null && counter < k){
+            if (actuel.getValeur() == v) {
+                actuel.suivante = actuel.suivante.suivante;
+                counter++;
+            }
+
+        }
+        return counter;
+    }
+
+    public int supprimer_k_premieres_occRec(int k, int v){
+       if (k == 0) {
+           return 0;
+       }
+       if (this.getSuivante().getSuivante() == null) {
+           if (this.getSuivante().getValeur() == v) {
+               this.setSuivante(this.getSuivante().getSuivante());
+               return 1;
+           }
+           return 0;
+       }
+       if (this.getSuivante().getValeur() == v) {
+            this.setSuivante(this.getSuivante().getSuivante());
+           return 1 + this.getSuivante().supprimer_k_premieres_occRec(k-1,v);
+       }
+       return this.getSuivante().supprimer_k_premieres_occRec(k,v);
+    }
+
+    public void swap(int i,int j ){
+        Cellule actuelPositionI = this;
+        Cellule actuelPositionJ = this;
+        for(int k=0; k<i-1; k++){
+            if(actuelPositionI.suivante != null){
+                actuelPositionI = actuelPositionI.suivante;
+                continue;
+            }
+            return; //swap ne peut pas être possible
+        }
+        for(int k=0; k<j-1; k++){
+            if(actuelPositionJ.suivante != null){
+                actuelPositionJ = actuelPositionJ.suivante;
+                continue;
+            }
+            return; //swap ne peut pas être possible
+        }
+        int tmp = actuelPositionI.getValeur();
+        actuelPositionI.setValeur(actuelPositionJ.getValeur());
+        actuelPositionJ.setValeur(tmp);
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Cellule){
+            Cellule c = (Cellule) obj;
+            return (this.getValeur() == c.getValeur());
         }
         return false;
     }
-
-
-
-
-
-
 }
 
 
